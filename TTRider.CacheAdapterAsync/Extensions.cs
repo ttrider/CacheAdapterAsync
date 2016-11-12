0b0abe98f109fab5a2a9ae5b73ac732945e49b60
TTRider <software@ttrider.com>
2016-11-12 14:20:30 -0800

    initial code structure

---
+++ TTRider.CacheAdapterAsync/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Microsoft.Extensions.Caching
+{
+    public static class Extensions
+    {
+        public static Task<T> GetOrAdd<T>(this IMemoryCache cache, Func<Task<T>> factoryMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<T> GetOrAdd<T>(this IDistributedCache cache, Func<Task<T>> factoryMethod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
